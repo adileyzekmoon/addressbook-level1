4addb724adeef74771cad408ceac4fee0c896086
DESKTOP-ISL6DTP\Adil Eyzekmoon <adil.eyzekmoon@gmail.com>
2017-09-01 21:15:25 +0800

    added "sort" in scanner switch-case

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -373,6 +373,8 @@ public class AddressBook {
         final String commandType = commandTypeAndParams[0];
         final String commandArgs = commandTypeAndParams[1];
         switch (commandType) {
+            case COMMAND_SORT_WORD:
+                return executeSort();
         case COMMAND_ADD_WORD:
             return executeAddPerson(commandArgs);
         case COMMAND_FIND_WORD:
